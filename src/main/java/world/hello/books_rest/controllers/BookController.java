fd83df9032f7a9008e34381dc92b7028045538c8
Abhishek Shrestha <meabhi224@gmail.com>
2025-01-09 21:14:17 +0545

    changed Controller to RestController

--- src/main/java/world/hello/books_rest/controllers/BookController.java
+++ src/main/java/world/hello/books_rest/controllers/BookController.java
@@ -2,18 +2,18 @@ package world.hello.books_rest.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-// import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import world.hello.books_rest.domain.Book;
 import world.hello.books_rest.services.BookService;
 
-// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller
-// @RequestMapping
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
     private final BookService bookService;
 
@@ -21,10 +21,8 @@ public class BookController {
         this.bookService = bookService;
     }
 
-    @PostMapping("/books")
-    public ResponseEntity<Book> createBook(
-            // @PathVariable final String isbn,
-            @RequestBody final Book book) {
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody final Book book) {
         final Book savedBook = bookService.createBook(book);
         final var res = new ResponseEntity<Book>(savedBook, HttpStatus.CREATED);
         return res;
