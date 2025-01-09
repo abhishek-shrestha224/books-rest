4df29088ad5bed0ed6379039466cfd22a9ac54ac
Abhishek Shrestha <meabhi224@gmail.com>
2025-01-09 18:44:34 +0545

    init

---
+++ src/test/java/world/hello/books_rest/BooksRestApplicationTests.java
@@ -0,0 +1,13 @@
+package world.hello.books_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
