451c9e9dc4facdd3155d99ab943f999da1d6e6a1
Abhishek Shrestha <meabhi224@gmail.com>
2025-01-10 21:02:56 +0545

    added delete route handler

--- src/main/java/world/hello/books_rest/controllers/BookController.java
+++ src/main/java/world/hello/books_rest/controllers/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -109,4 +110,12 @@ public class BookController {
 
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") final String id) {
+        final boolean removed = bookService.deleteById(id);
+        if (!removed) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not Found :(");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body("Deleted :)");
+    }
 }
